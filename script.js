585302d6a984f6420f55d19f919348600db4bb93
João Pedro <fernandesjoaopedro622@gmail.com>
2025-12-14 15:08:14 -0300

    att

--- script.js
+++ script.js
@@ -934,7 +934,7 @@ document.addEventListener("DOMContentLoaded", function () {
                   value: dinheiroSujoDisplay,
                   inline: true,
                 },
-                { name: "📝 Detalhes", value: atenuantesTexto },
+                { name: "📝 Atenuantes/Outros", value: atenuantesTexto },
               ],
               footer: {
                 text:
